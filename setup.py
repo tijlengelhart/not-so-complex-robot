2e0f2d86a69b873b05af378df6d3f612bc47f056
tijlengelhart <82236683+tijlengelhart@users.noreply.github.com>
2021-07-29 11:37:17 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#pip import all the packages wich are: datetime, webbrowser, subprocesss, pyjokes, pyttsx3, speech_recognition
+#paste the code into your code edittor
+#run the code
+
+#for the _fileopen this CAN differ per computer so if you want to change your path to open up the file just change the path that is displayed in line: 84 and 87
+
+#run the code...
+
+#enjoy your new friend!
